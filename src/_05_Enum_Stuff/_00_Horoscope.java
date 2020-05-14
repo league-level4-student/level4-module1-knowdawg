4d7176aa416b6feee6053eee880790875c2eb594
root <root@d61541c3e8f9>
2020-05-14 01:21:51 +0000

    snake IP

--- src/_05_Enum_Stuff/_00_Horoscope.java
+++ src/_05_Enum_Stuff/_00_Horoscope.java
@@ -1,5 +1,7 @@
 package _05_Enum_Stuff;
 
+import java.util.Random;
+
 import javax.swing.JOptionPane;
 
 public class _00_Horoscope {
@@ -9,6 +11,58 @@ public class _00_Horoscope {
 	// 2. Write a method that takes in a Zodiac enum object and uses a JOPtionPane to display
 	//    a different horoscope based on the Zodiac's state.
 	
+	public static void pickHoroscope() {
+		Random picker = new Random();
+		int state = picker.nextInt(12);
+		
+		switch(state){
+		case 0:
+			JOptionPane.showMessageDialog(null, Zodiac.ARIES);
+			break;
+		case 1: 
+			JOptionPane.showMessageDialog(null, Zodiac.TAURUS);
+			break;
+		case 2:
+			JOptionPane.showMessageDialog(null, Zodiac.GEMINI);
+			break;
+		case 3:
+			JOptionPane.showMessageDialog(null, Zodiac.CANCER);
+			break;
+		case 4: 
+			JOptionPane.showMessageDialog(null, Zodiac.LEO);
+			break;
+		case 5:
+			JOptionPane.showMessageDialog(null, Zodiac.VIRGO);
+			break;
+		case 6:
+			JOptionPane.showMessageDialog(null, Zodiac.LIBRA);
+			break;
+		case 7:
+			JOptionPane.showMessageDialog(null, Zodiac.SCORPIO);
+			break;
+		case 8:
+			JOptionPane.showMessageDialog(null, Zodiac.SAGITTARIUS);
+			break;
+		case 9:
+			JOptionPane.showMessageDialog(null, Zodiac.CAPRICORN);
+			break;
+		case 10:
+			JOptionPane.showMessageDialog(null, Zodiac.AQUARIUS);
+			break;
+		case 11:
+			JOptionPane.showMessageDialog(null, Zodiac.PISCES);
+			break;
+			
+		}
+		
+		
+	}
+		
+
 	// 3. Make a main method to test your method
+	public static void main(String[] args) {
+		pickHoroscope();
+	}
+	
 	
 }
--- src/_06_Snake/_00_SnakeGame.java
+++ src/_06_Snake/_00_SnakeGame.java
@@ -86,7 +86,17 @@ public class _00_SnakeGame implements ActionListener, KeyListener {
 		//   Use timer.setDelay(delay) with different numbers to change the speed
 		//   of the game. The smaller the number, the faster it goes.
 
+		switch(choice) {
+		case "Expert":
+			timer.setDelay(20);
+		case "Moderate":
+			timer.setDelay(40);
+		case "Beginner":
+			timer.setDelay(60);
+		}
+			
 		//3. start the timer
+		timer.start();
 	}
 
 	public static void main(String[] args) {
@@ -103,7 +113,10 @@ public class _00_SnakeGame implements ActionListener, KeyListener {
 	public void keyPressed(KeyEvent e) {
 		//1. Use a switch statement on e.getKeyCode()
 		//   to determine which key was pressed.
-		
+		switch(e.getKeyCode()){
+			
+			
+		}
 		// if an arrow key is pressed, set the snake's 
 		// direction accordingly
 		
